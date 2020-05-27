8cd05a4c0b3635c476419bc1b1e5cd3165644458
itotallyrock <itotallyrock@users.noreply.github.com>
2020-05-27 08:22:06 -0500

    Fix missing castle case in from string and use full name rather than union for match

--- src/castles.rs
+++ src/castles.rs
@@ -6,14 +6,14 @@ use super::errors;
 bitflags! {
     pub struct CastlePermissions: u8 {
         const NONE = 0;
-        const WHITE_KING_CASTLE = 1;
-        const WHITE_QUEEN_CASTLE = 2;
+        const WHITE_KING = 1;
+        const WHITE_QUEEN = 2;
         const WHITE_ALL = 3;
-        const BLACK_KING_CASTLE = 4;
+        const BLACK_KING = 4;
         const BOTH_KINGS = 5;
         const WHITE_QUEEN_BLACK_KING = 6;
         const WHITE_ALL_BLACK_KING = 7;
-        const BLACK_QUEEN_CASTLE = 8;
+        const BLACK_QUEEN = 8;
         const WHITE_KING_BLACK_QUEEN = 9;
         const BOTH_QUEENS = 10;
         const WHITE_ALL_BLACK_QUEEN = 11;
@@ -33,14 +33,14 @@ impl Default for CastlePermissions {
 impl ToString for CastlePermissions {
     fn to_string(&self) -> String {
         match self.clone() {
-            CastlePermissions::WHITE_KING_CASTLE => "K".to_string(),
-            CastlePermissions::WHITE_QUEEN_CASTLE => "Q".to_string(),
+            CastlePermissions::WHITE_KING => "K".to_string(),
+            CastlePermissions::WHITE_QUEEN => "Q".to_string(),
             CastlePermissions::WHITE_ALL => "KQ".to_string(),
-            CastlePermissions::BLACK_KING_CASTLE => "k".to_string(),
+            CastlePermissions::BLACK_KING => "k".to_string(),
             CastlePermissions::BOTH_KINGS => "Kk".to_string(),
             CastlePermissions::WHITE_QUEEN_BLACK_KING => "Qk".to_string(),
             CastlePermissions::WHITE_ALL_BLACK_KING => "KQk".to_string(),
-            CastlePermissions::BLACK_QUEEN_CASTLE => "q".to_string(),
+            CastlePermissions::BLACK_QUEEN => "q".to_string(),
             CastlePermissions::WHITE_KING_BLACK_QUEEN => "Kq".to_string(),
             CastlePermissions::BOTH_QUEENS => "Qq".to_string(),
             CastlePermissions::WHITE_ALL_BLACK_QUEEN => "KQq".to_string(),
@@ -59,20 +59,21 @@ impl TryFrom<String> for CastlePermissions {
         match castle_string.as_str() {
             "KQkq" => Ok(CastlePermissions::ALL),
 
-            "KQk" => Ok(CastlePermissions::WHITE_ALL | CastlePermissions::BLACK_KING_CASTLE),
-            "KQq" => Ok(CastlePermissions::WHITE_ALL | CastlePermissions::BLACK_QUEEN_CASTLE),
-            "Kkq" => Ok(CastlePermissions::WHITE_KING_CASTLE | CastlePermissions::BLACK_ALL),
-            "Qkq" => Ok(CastlePermissions::WHITE_QUEEN_CASTLE | CastlePermissions::BLACK_ALL),
+            "KQk" => Ok(CastlePermissions::WHITE_ALL_BLACK_KING),
+            "KQq" => Ok(CastlePermissions::WHITE_ALL_BLACK_QUEEN),
+            "Kkq" => Ok(CastlePermissions::BLACK_ALL_WHITE_KING),
+            "Qkq" => Ok(CastlePermissions::BLACK_ALL_WHITE_QUEEN),
 
             "KQ" => Ok(CastlePermissions::WHITE_ALL),
             "kq" => Ok(CastlePermissions::BLACK_ALL),
             "Kk" => Ok(CastlePermissions::BOTH_KINGS),
             "Qq" => Ok(CastlePermissions::BOTH_QUEENS),
+            "Qk" => Ok(CastlePermissions::WHITE_QUEEN_BLACK_KING),
 
-            "K" => Ok(CastlePermissions::WHITE_KING_CASTLE),
-            "Q" => Ok(CastlePermissions::WHITE_QUEEN_CASTLE),
-            "k" => Ok(CastlePermissions::BLACK_KING_CASTLE),
-            "q" => Ok(CastlePermissions::BLACK_QUEEN_CASTLE),
+            "K" => Ok(CastlePermissions::WHITE_KING),
+            "Q" => Ok(CastlePermissions::WHITE_QUEEN),
+            "k" => Ok(CastlePermissions::BLACK_KING),
+            "q" => Ok(CastlePermissions::BLACK_QUEEN),
 
             "-" => Ok(CastlePermissions::NONE),
             _ => Err(errors::InvalidCastlesError),
