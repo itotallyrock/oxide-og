e16e52869e71403ae43a2666dac9e0e7151cfc47
itotallyrock <itotallyrock@users.noreply.github.com>
2020-05-30 01:19:19 -0500

    Add builder style creation for chess_move.rs, added Debug, and Eq, and cleaned up namespaces

--- src/chess_move.rs
+++ src/chess_move.rs
@@ -1,9 +1,9 @@
 
-use super::pieces;
+use super::square::Square;
+use super::castles::CastlePermissions;
+use super::pieces::{PieceRepr, Piece};
+use super::side::Side;
 use super::square;
-use super::castles;
-use super::side;
-use crate::pieces::PieceRepr;
 
 /// Allows converting move to SAN
 pub trait SanMove {
@@ -19,43 +19,135 @@ pub trait UCIMove {
 
 /// Move associated data irrespective of board state
 /// Reversible entirely
+#[derive(Clone, Debug, PartialEq, Eq)]
 pub struct Move {
     /// Side doing the move
-    pub side: side::Side,
+    pub side: Side,
     /// What piece is moving
-    pub piece: pieces::Piece,
+    pub piece: Piece,
     /// What square is being moved from
-    pub from: square::Square,
+    pub from: Square,
     /// What square is being moved to
-    pub to: square::Square,
+    pub to: Square,
     /// What piece is being captured
-    pub captured_piece: Option<pieces::Piece>,
+    pub captured_piece: Option<Piece>,
     /// What piece is this being promoted to
-    pub promoted_piece: Option<pieces::Piece>,
+    pub promoted_piece: Option<Piece>,
     /// What en passant square is left after this move
-    pub enpassant_square: Option<square::Square>,
+    pub enpassant_square: Option<Square>,
     /// Which castles were used in the move (not the permissions before or after move)
-    pub castles_used: castles::CastlePermissions,
+    pub castles_used: CastlePermissions,
     /// If the attack was on an en passant pawn
     pub enpassant_capture: bool,
 }
 
 impl Move {
-    pub fn new_castle_permissions(&self, current_rights: castles::CastlePermissions) -> castles::CastlePermissions {
-        let mut new_rights: castles::CastlePermissions = current_rights;
-        if self.piece == pieces::Piece::Rook {
+
+    pub fn new(side: Side, piece: Piece, from: Square, to: Square) -> Move {
+        Move {
+            side,
+            piece,
+            from,
+            to,
+            captured_piece: None,
+            promoted_piece: None,
+            enpassant_square: None,
+            castles_used: Default::default(),
+            enpassant_capture: false
+        }
+    }
+
+    pub fn capture(&mut self, captured_piece: Piece) -> &mut Self {
+        self.captured_piece = Some(captured_piece);
+        self
+    }
+
+    pub fn promote(&mut self, promoted_piece: Piece) -> &mut Self {
+        self.promoted_piece = Some(promoted_piece);
+        self
+    }
+
+    pub fn en_passant_capture(&mut self) -> &mut Self {
+        self.captured_piece = Some(Piece::Pawn);
+        self.enpassant_capture = true;
+        self
+    }
+
+    #[inline]
+    pub fn white_king_castle() -> Self {
+        Move {
+            side: Side::White,
+            piece: Piece::King,
+            from: square::named::E1.into(),
+            to: square::named::G1.into(),
+            captured_piece: None,
+            promoted_piece: None,
+            enpassant_square: None,
+            castles_used: CastlePermissions::WHITE_KING,
+            enpassant_capture: false
+        }
+    }
+
+    #[inline]
+    pub fn white_queen_castle() -> Self {
+        Move {
+            side: Side::White,
+            piece: Piece::King,
+            from: square::named::E1.into(),
+            to: square::named::C1.into(),
+            captured_piece: None,
+            promoted_piece: None,
+            enpassant_square: None,
+            castles_used: CastlePermissions::WHITE_QUEEN,
+            enpassant_capture: false
+        }
+    }
+
+    #[inline]
+    pub fn black_king_castle() -> Self {
+        Move {
+            side: Side::Black,
+            piece: Piece::King,
+            from: square::named::E8.into(),
+            to: square::named::G8.into(),
+            captured_piece: None,
+            promoted_piece: None,
+            enpassant_square: None,
+            castles_used: CastlePermissions::BLACK_KING,
+            enpassant_capture: false
+        }
+    }
+
+    #[inline]
+    pub fn black_queen_castle() -> Self {
+        Move {
+            side: Side::Black,
+            piece: Piece::Queen,
+            from: square::named::E8.into(),
+            to: square::named::C8.into(),
+            captured_piece: None,
+            promoted_piece: None,
+            enpassant_square: None,
+            castles_used: CastlePermissions::BLACK_QUEEN,
+            enpassant_capture: false
+        }
+    }
+
+    pub fn new_castle_permissions(&self, current_rights: CastlePermissions) -> CastlePermissions {
+        let mut new_rights: CastlePermissions = current_rights;
+        if self.piece == Piece::Rook {
             match self.from.offset {
-                square::named::H1 if self.side == side::Side::White => { new_rights &= castles::CastlePermissions::BLACK_ALL_WHITE_QUEEN; },
-                square::named::A1 if self.side == side::Side::White => { new_rights &= castles::CastlePermissions::BLACK_ALL_WHITE_KING; },
-                square::named::H8 if self.side == side::Side::Black => { new_rights &= castles::CastlePermissions::WHITE_ALL_BLACK_QUEEN; },
-                square::named::A8 if self.side == side::Side::Black => { new_rights &= castles::CastlePermissions::WHITE_ALL_BLACK_KING; },
+                square::named::H1 if self.side == Side::White => { new_rights &= CastlePermissions::BLACK_ALL_WHITE_QUEEN; },
+                square::named::A1 if self.side == Side::White => { new_rights &= CastlePermissions::BLACK_ALL_WHITE_KING; },
+                square::named::H8 if self.side == Side::Black => { new_rights &= CastlePermissions::WHITE_ALL_BLACK_QUEEN; },
+                square::named::A8 if self.side == Side::Black => { new_rights &= CastlePermissions::WHITE_ALL_BLACK_KING; },
                 _ => { },
             }
-        } else if !self.castles_used.is_empty() || self.piece == pieces::Piece::King {
-            if self.side == side::Side::White {
-                new_rights &= castles::CastlePermissions::BLACK_ALL;
+        } else if !self.castles_used.is_empty() || self.piece == Piece::King {
+            if self.side == Side::White {
+                new_rights &= CastlePermissions::BLACK_ALL;
             } else {
-                new_rights &= castles::CastlePermissions::WHITE_ALL;
+                new_rights &= CastlePermissions::WHITE_ALL;
             }
         }
 
