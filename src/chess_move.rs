a4d196c293d5b18830859b4a15af8d50c3f8b0ad
itotallyrock <itotallyrock@users.noreply.github.com>
2020-10-17 23:04:21 -0500

    Minor chess move display bugfix and convenience functions for it as well

--- src/chess_move.rs
+++ src/chess_move.rs
@@ -2,6 +2,7 @@
 use crate::pieces::Piece;
 use crate::square::Square;
 use std::fmt::{Display, Formatter, Result as FormatResult};
+use crate::square::named::{E1, G1, C1, E8, G8, C8};
 
 #[repr(u8)]
 #[derive(Copy, Clone, Eq, PartialEq, Debug)]
@@ -54,10 +55,23 @@ impl Flags {
             Flags::QueenPromotion   | Flags::QueenPromotingCapture  => Flags::QueenPromotingCapture,
         }
     }
+    pub(crate) fn remove_capture(&self) -> Self {
+        match self {
+            Flags::Quiet | Flags::Capture | Flags::EnPassantCapture => Flags::Quiet,
+            Flags::DoublePawnPush => Flags::DoublePawnPush,
+            Flags::KingSideCastle => Flags::KingSideCastle,
+            Flags::QueenSideCastle => Flags::QueenSideCastle,
+            Flags::KnightPromotion | Flags::KnightPromotingCapture => Flags::KnightPromotion,
+            Flags::BishopPromotion | Flags::BishopPromotingCapture => Flags::BishopPromotion,
+            Flags::RookPromotion | Flags::RookPromotingCapture => Flags::RookPromotion,
+            Flags::QueenPromotion | Flags::QueenPromotingCapture => Flags::QueenPromotion,
+        }
+    }
 }
 
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
 pub struct ChessMove(pub Square, pub Square, pub Flags);
+
 
 impl Display for ChessMove {
     fn fmt(&self, f: &mut Formatter<'_>) -> FormatResult {
@@ -67,14 +81,31 @@ impl Display for ChessMove {
             // NOTE: Potentially handle differently (chess960 or o-o notation are concerns worth investigating)
             Flags::KingSideCastle | Flags::QueenSideCastle => write!(f, "{}{}", from, to),
             Flags::Capture | Flags::EnPassantCapture => write!(f, "{}x{}", from, to),
-            Flags::KnightPromotion => write!(f, "{}{}k", to, from),
-            Flags::BishopPromotion => write!(f, "{}{}b", to, from),
-            Flags::RookPromotion => write!(f, "{}{}r", to, from),
-            Flags::QueenPromotion => write!(f, "{}{}q", to, from),
-            Flags::KnightPromotingCapture => write!(f, "{}x{}k", to, from),
-            Flags::BishopPromotingCapture => write!(f, "{}x{}b", to, from),
-            Flags::RookPromotingCapture => write!(f, "{}x{}r", to, from),
-            Flags::QueenPromotingCapture => write!(f, "{}x{}q", to, from),
+            Flags::KnightPromotion => write!(f, "{}{}n", from, to),
+            Flags::BishopPromotion => write!(f, "{}{}b", from, to),
+            Flags::RookPromotion => write!(f, "{}{}r", from, to),
+            Flags::QueenPromotion => write!(f, "{}{}q", from, to),
+            Flags::KnightPromotingCapture => write!(f, "{}x{}n", from, to),
+            Flags::BishopPromotingCapture => write!(f, "{}x{}b", from, to),
+            Flags::RookPromotingCapture => write!(f, "{}x{}r", from, to),
+            Flags::QueenPromotingCapture => write!(f, "{}x{}q", from, to),
         }
+    }
+}
+
+impl ChessMove {
+    pub const WHITE_KING_CASTLE: ChessMove = ChessMove(E1, G1, Flags::KingSideCastle);
+    pub const WHITE_QUEEN_CASTLE: ChessMove = ChessMove(E1, C1, Flags::QueenSideCastle);
+    pub const BLACK_KING_CASTLE: ChessMove = ChessMove(E8, G8, Flags::KingSideCastle);
+    pub const BLACK_QUEEN_CASTLE: ChessMove = ChessMove(E8, C8, Flags::QueenSideCastle);
+
+    pub fn from(&self) -> Square {
+        self.0
+    }
+    pub fn to(&self) -> Square {
+        self.1
+    }
+    pub fn flags(&self) -> Flags {
+        self.2
     }
 }
